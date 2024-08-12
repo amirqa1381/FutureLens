59076f68466cfc1cfb56d10a8690b31f88800fd4
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-08-12 19:15:52 +0330

    these two files are view and index that we want to show the chart to the page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import urllib
+import base64
 
-# Create your views here.
+
+def index_view(request: HttpRequest):
+    """
+    this function is for the index view and we can show the all the chart on it for the starting
+    Args:
+        request (HttpRequest): this is the request that we pass to this function
+    """
+    # sample data
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+
+    # create the plot
+    plt.figure()
+    plt.plot(x, y)
+    plt.title("Sine Wave")
+    plt.xlabel("x-axis")
+    plt.ylabel("Y-axis")
+
+    # save the plot to the bytesIO object
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    plt.close()
+    buf.seek(0)
+
+    # Encode the image to base64
+    image_png = base64.b64encode(buf.read())
+    buf.close()
+    image_str = image_png.decode("utf-8")
+    context = {"chart": image_str}
+    return render(request, "main/index.html", context)
