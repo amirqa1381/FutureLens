c5255ed73c727776428deb2238b368bdfc1fa95d
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-08-17 13:18:18 +0330

    Added the ShheowTheChart view to the urls for handling the route

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ShowTheChart
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('chart/', ShowTheChart.as_view(), name='show_chart'),
 ]
