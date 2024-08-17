a9f910f2d286abfadc3e0f1a3c2f3c41f097f581
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-08-17 11:34:43 +0330

    here in this file i remove the two line of it and remove the show and getting-file path

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import index_view, read, getting_form
+from .views import IndexView
 
 
 urlpatterns = [
-    path('', index_view, name='index'),
-    path('show/', read), 
-    path('getting-file/', getting_form, name='getting_file'),
+    path('', IndexView.as_view(), name='index'),
 ]
