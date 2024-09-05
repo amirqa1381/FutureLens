c661a0729bd3935422aaee649c826cabd90f963f
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-09-05 09:39:17 +0330

    here we added somemethod in the MIssingValue class

--- data_code/EDR/missing_data.py
+++ data_code/EDR/missing_data.py
@@ -19,14 +19,13 @@ class MissingValue:
 
     def data_show_head(self, row: int = None):
         """
-        this function is for a time that we want to showing the data frame to the user and now 
-        if user want to see the more than the default of the head , it can pass a number as input to the 
+        this function is for a time that we want to showing the data frame to the user and now
+        if user want to see the more than the default of the head , it can pass a number as input to the
         function
         Args:
             row (int, optional): a number can pass to the function for receving the dataframe row
         """
         return self.data.head(row)
-    
 
     def data_frame_info(self):
         """
@@ -45,7 +44,7 @@ class MissingValue:
         this function is for a time that we want to get the sum of the nall fields
         """
         return self.data.isna().sum()
-    
+
     def data_unique_value(self, column: str):
         """
         this function is for retriving the unique value that is in the each column of the dataframe
@@ -57,12 +56,42 @@ class MissingValue:
             return result
         except:
             print("Missing some value")
-    
+
+    def find_data_isna_column(self, coulmn: str):
+        """
+        this function is for a time that we want to finding the isna from specific column
+        Args:
+            coulmn (str): specify the name of the column that we want to find its nall
+        """
+        result = self.data[self.data[coulmn].isna()]
+        return result
+
+    def fill_isna_column_max(self, column: str) -> None:
+        """
+        this method is for a time that we want to fill the isna column with max value of that coulmn
+        Args:
+            column (str): this is the column that we want to fill the isna
+        """
+        self.data[column] = self.data[column].fillna(self.data[column].max())
+
+    def fill_isna_column_string_type(self, column: str) -> None:
+        self.data[column] = self.data[column].fillna("")
+
+    def data_columns(self):
+        """
+        this method is for a time that we want to bring all the columns
+        """
+        return self.data.columns
+
+    def fill_all_isna_columns(self):
+        """
+        in this method we fill all the isna clumns , if coulmn was bifc we fill it with max valure
+        and if the column was string we fill it with empty qutation
+        """
+        for column in self.data_columns():
+            if self.data[column].dtypes.kind == "bifc":
+                self.fill_isna_column_max(column)
+            else:
+                self.fill_isna_column_string_type(column)
 
 
-file = r"../../datas/Cust_Segmentation.csv"
-missing = MissingValue(file=file)
-# print(missing.data_frame_info())
-# print(missing.data_isna_sum())
-# print(missing.data_show_head(5))
-print(missing.data_unique_value())
