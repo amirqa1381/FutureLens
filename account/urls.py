45a8176a46991efcdb2219e12c29c0b0f38b4b0b
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-09-13 10:29:38 +0330

    in the urls i've added the UserLoginView for routing to this view

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserLoginView
 
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="user_register_view"),
+    path("login/", UserLoginView.as_view(), name="user_login_view"),
 ]
