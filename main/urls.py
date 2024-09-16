a1bfb6dfc6ab22997f798ec3bcafd98de573f7cd
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-09-16 11:49:27 +0330

    added the uploade class to the url for routing the request of the user to this view for handling it

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import IndexView, ShowTheDataFrame, ShowThePlot
+from .views import (IndexView,
+                    ShowTheDataFrame,
+                    ShowThePlot,
+                    UploadTheFile,
+                    )
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('data-frame/', ShowTheDataFrame.as_view(), name='data-frame'),
     path('plot/', ShowThePlot.as_view(), name='show_plot'),
+    path("upload-file/", UploadTheFile.as_view(), name="uploading_file"),
 ]
