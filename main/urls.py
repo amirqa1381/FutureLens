e411d0dcf217c4d95ae894d170e46917da25dd6a
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-09-17 10:51:22 +0330

    here i have added the slug to the url and added it to the view for routing it

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,7 @@ from .views import (IndexView,
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('data-frame/<slug:slug>', ShowTheDataFrame.as_view(), name='data-frame'),
-    path('plot/', ShowThePlot.as_view(), name='show_plot'),
+    path('plot/<slug:slug>', ShowThePlot.as_view(), name='show_plot'),
     path("upload-file/", UploadTheFile.as_view(), name="uploading_file"),
     path("datas-list/", UserDatasListView.as_view(), name="data-list")
 ]
