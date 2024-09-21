34d5f2fd60d820095f21591b5cc889a070745dca
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-09-21 16:15:22 +0330

    i have defined new function for finding the result of the search part

--- search/views.py
+++ search/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.db.models import Q
 
-# Create your views here.
+
+def search_view(request: HttpRequest):
+    """
+    View function for handling search queries.
+    Args:
+        request (HttpRequest): _description_
+    """
+    if  request.method == "POST":
+        searched = request.POST.get('searched', None)
+        results = User.userfiles_set.filter(Q(title__icontains=searched) | Q(slug__containes=searched) & Q(cleaned=True))
+        if results is not None:
+            context = {
+            'results': results
+            }
+        else:
+            context = {"text": "Nothing found!!!!!"}
+        return render(request, "search/search.html", context)
+        
+        
