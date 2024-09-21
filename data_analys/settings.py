db38a3e2d8f41d005f311e84ba301d5c93279ff5
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-09-21 16:57:51 +0330

    here i have added the staticfiles_dir

--- data_analys/settings.py
+++ data_analys/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # this is the media rout
 MEDIA_URL = '/media/'
