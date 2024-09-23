6814a2fa8d73a14fcdbb297600d5c48c09f6a26e
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-09-23 16:49:23 +0330

    here i have defined two function that are for getting and passing the token and finding the result base on the query that they have

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from django.db.models import Q
+from .tokens import generate_token, retrive_query_from_token
 
 
 def search_view(request: HttpRequest):
@@ -9,15 +10,39 @@ def search_view(request: HttpRequest):
     Args:
         request (HttpRequest): _description_
     """
-    if  request.method == "POST":
-        searched = request.POST.get('searched', None)
-        results = request.user.userfiles_set.filter(Q(title__icontains=searched) | Q(slug__icontains=searched) & Q(cleaned=True))
-        if results is not None:
-            context = {
-            'results': results
-            }
-        else:
-            context = {"text": "Nothing found!!!!!"}
-        return render(request, "search/search.html", context)
+    if request.method == "POST":
+        query = request.POST.get("searched", None)
+        token = generate_token(query)
+        return redirect("", args=[token])
+    
+
+def search_result(request: HttpRequest, token):
+    """
+    this funciton is for the finding the result base of the token that is provided
+    Args:
+        request (HttpRequest): _description_
+        token (_type_): _description_
+    """
+    query = retrive_query_from_token(token=token)
+    results = request.user.userfiles_set.filter(Q(title__icontains=query) | Q(slug__icontains=query) & Q(cleaned=True))
+    context = {
+        "results": results
+    }
+    return render(request, "search/search.html", context)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
