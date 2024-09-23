e3e4920ee84f5e6888daad28c29cd57dce94c809
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-09-23 16:48:28 +0330

    here i have commented the class form that i have desined

--- search/forms.py
+++ search/forms.py
@@ -1,11 +1,23 @@
 from django import forms
 
-class SearchForm(forms.Form):
-    """
-    this class is for the search form and here every user can write all the things that want 
-    and we proceed it for the user and we find and retrive the result fot the user
-    Args:
-        forms (_type_): _description_
-    """
-    query = forms.CharField(max_length=150, verbose_name="Query")
-    
+
+# class SearchForm(forms.Form):
+#     """
+#     this class is for the search form and here every user can write all the things that want
+#     and we proceed it for the user and we find and retrive the result fot the user
+#     Args:
+#         forms (_type_): _description_
+#     """
+
+#     query = forms.CharField(
+#         max_length=150,
+#         widget=forms.TextInput(
+#             attrs={
+#                 "class": "form-control me-2",
+#                 "type": "search",
+#                 "placeholder": "Search",
+#                 "aria-label": "Search",
+#                 "name": "searched",
+#             }
+#         ),
+#     )
