b432629e7d56a5dfd7644b5635188cd8cb254e82
Amir Hussein <amirghasemian1381@gmail.com>
2024-11-13 14:23:33 +0330

    correct the word

--- main/views.py
+++ main/views.py
@@ -40,7 +40,7 @@ class IndexView(View):
 class UploadTheFile(LoginRequiredMixin, FormView):
     """
     this is a class that is for the uploading the file and this class handle it for us
-    and only authenticated users can upload the file here and anonymos user can not do that
+    and only authenticated users can upload the file here and anonyms user can not do that
     """
 
     template_name = "main/upload-file.html"
@@ -72,7 +72,7 @@ class ShowTheDataFrame(View):
         Args:
             request (HttpRequest): _description_
         """
-        # here we find the file that we user pass the slug from it for finding and retriving it
+        # here we find the file that we user pass the slug from it for finding and retrieving it
         file = request.user.userfiles_set.get(slug=slug, user=request.user)
         csv_reader = CSVReader(file.file.path)
         data_info = csv_reader.data_info()
@@ -87,7 +87,7 @@ class ShowTheDataFrame(View):
 
     def post(self, request: HttpRequest):
         """
-        this is a function that is for handling the post request and if user submit simething it will handle
+        this is a function that is for handling the post request and if user submit something it will handle
         it....
         Args:
             request (HttpRequest): _description_
@@ -102,7 +102,7 @@ class ShowTheDataFrame(View):
 class ShowThePlot(View):
     """
     this class is for showing the plots in the page that we  want for better understanding
-    and seperate them from the dataframe
+    and seperate them from the data frame
     Args:
         View (django.views): this is the views
     """
@@ -123,7 +123,7 @@ class ShowThePlot(View):
 
     def post(self, request: HttpRequest, **kwargs):
         """
-        this is the method in the class that is for the handling the post requesst
+        this is the method in the class that is for the handling the post requests
 
         Args:
             request (HttpRequest): _description_
@@ -176,7 +176,7 @@ class ImplementingMissingAndFixingData(LoginRequiredMixin, View):
     def post(self, request: HttpRequest, **kwargs):
         """
         this function is for handling the post method that we have for fixing the data that we have
-        and when user send the post request to it this class is for hanling it
+        and when user send the post request to it this class is for handling it
         Args:
             request (HttpRequest): _description_
             slug (_type_): _description_
@@ -190,7 +190,7 @@ class ImplementingMissingAndFixingData(LoginRequiredMixin, View):
         print(f"After {missing.data_isna_sum()}")
 
         if fixed_file is not None:
-            # update the dataframe
+            # update the data frame
             fixed_data = fixed_file
             
             # here we open the original file and rewrite the cleaned data frame on it
